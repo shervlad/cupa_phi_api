327fda8b0a0d9a7c241a268241a620871acbc38e
Vlad Seremet <vladseremet@gmail.com>
2020-08-24 23:28:27 +0300

    define register and join methods

--- app.py
+++ app.py
@@ -7,13 +7,8 @@ from questions import questions
 from flask import jsonify
 import random
 import string
-import Game from './game.py'
+from game import Game
 
-def get_random_string(length):
-    letters = string.ascii_lowercase
-    result_str = ''.join(random.choice(letters) for i in range(length))
-
-    return result_str
 
 class Team:
     def __init__(self):
@@ -26,41 +21,78 @@ g = Game(questions)
 app = flask.Flask(__name__)
 CORS(app)
 app.config["DEBUG"] = True
+ADMIN_CODE = "i_am_the_admin"
 
+@app.route('/get_status/<teamId>', methods=['GET'])
+def getStatus(teamId):
+    return jsonify(g.getStatus(teamId))
 
-@app.route('/get_status', methods=['GET'])
-def getStatus():
-    return g.getStatus()
-
-@app.route('/get_questions', methods=['GET'])
+@app.route('/get_questions/<team_id>', methods=['GET'])
 def getQuestions():
     return jsonify(g.questions_list)
 
-@app.route('/send_answer/<team_id>/<question_id>/<answer>', methods=['GET'])
-def sendAnswer(team_id,question_id,answer):
-    verification_queue.append([team_id,question_id,answer])
+@app.route('/send_answer/<team_id>/<member_name>/<question_id>/<answer>', methods=['GET'])
+def sendAnswer(team_id,member_name,question_id,answer):
+    verification_queue.append([team_id,member_name,question_id,answer])
 
-@app.route('/get_team_code/<team_name>', methods=['GET'])
-def generateTeamCode(team_code):
-    for t in g.rankings:
-        if(t['teamName'] == team_name):
-            return {'status' :-1,'message': 'team with this name already exists'}
-    teamId = get_random_string(5)
-    while(teamId in g.rankings):
-        teamId = get_random_string(5)
+@app.route('/register_team/<team_name>', methods=['GET'])
+def registerTeam(team_name):
+    return jsonify(g.registerTeam(team_name))
 
-    g.rankings[teamId] = {
-        'teamId'   : teamId,
-        'teamName' : team_name,
-        'record'   : [0 for q in g.questions_list],
-    }
+@app.route('/join/<team_id>', methods=['GET'])
+def join(team_id):
+    if(teamId == this.admin_code):
+        return jsonify({'success':1,'data':g.getAdminStatus(team_id)})
 
-@app.route('/start_game', methods=['GET'])
-def startGame():
-    g.start()
-    return {'status':'game_started'}
+    if(team_id not in g.history):
+        return jsonify({
+            'success' : -1,
+            'message': 'team with this ID not registered'
+        })
+    else:
+        return jsonify({'success':1,'data':g.getStatus(team_id)})
+
+@app.route('/start_game/<admin_code>', methods=['GET'])
+def startGame(admin_code):
+    if(admin_code == ADMIN_CODE):
+        g.start()
+        return {'status': 1,
+                'message':'game_started'}
+    else:
+        return {'status':-1,
+                'message': 'you do not have authorization to perform this operation'}
 
 
+@app.route('/get_verification_queue/<admin_code>', methods=['POST'])
+def getVerificationQueue(admin_code):
+    if(admin_code == ADMIN_CODE):
+        data = g.getVerificationQueue()
+        return {'status': 1,
+                'data': data}
+    else:
+        return {'status':-1,
+                'message': 'you do not have authorization to perform this operation'}
+
+@app.route('/verify_answer/<admin_code>', methods=['POST'])
+def verifyAnswer(admin_code):
+    if(admin_code == ADMIN_CODE):
+        submission = {
+        'teamId'     : request.form['teamId'],
+        'questionId' : request.form['questionId'],
+        'check'      : request.form['check']
+
+        }
+        g.verify(submission)
+        return {'status': 1,
+                'message':'verified!'}
+    else:
+        return {'status':-1,
+                'message': 'you do not have authorization to perform this operation'}
+
+
+@app.route('/getRankings', methods=['GET'])
+def getRankings():
+    return g.getRankings()
 
 
 
--- game.py
+++ game.py
@@ -1,3 +1,7 @@
+import random
+import string
+import threading
+
 class Game:
     def __init__(self, questions_list):
         self.status = 'not_started'
@@ -11,10 +15,12 @@ class Game:
         self.waiting_timer = -1
         self.questions_list = questions_list
 
-        self.rankings = {
+        self.game_thread = None
+
+        self.history = {
             'team1id' : {
-                    'teamId'   : 'asox023qe32nsdfsq', #some random string
-                    'teamName' : 'The Bilzebobs',
+                    'team_id'   : 'asox023qe32nsdfsq', #some random string
+                    'team_name' : 'The Bilzebobs',
                     'record'   : {'q1_id' : 0, 'q2_id' : 1, 'q3_id': 1, 'q4_id':0},
                     'answers'  : {
                         'q1_id' : { 'raspuns': 'iarba verde', 'team_member': 'vasilika'},
@@ -26,7 +32,23 @@ class Game:
                 }
         }
 
-    def getStatus(self):
+        self.rankings = {
+            'team1' : [0,0,1,0,1,0,0,1,0,1,0,-1,-1,-1,-1,-1],
+            'team2' : [0,1,9,1,1,0,0,1,0,1,0,-1,-1,-1,-1,-1],
+            'team3' : [0,0,1,1,1,1,0,1,0,1,0,-1,-1,-1,-1,-1],
+            'team4' : [0,0,1,0,1,0,1,1,0,1,1,-1,-1,-1,-1,-1],
+            'team5' : [0,0,1,0,1,0,0,1,0,1,0,-1,-1,-1,-1,-1]
+        }
+
+        self.verification_queue = []
+
+    def get_random_string(self, length):
+        letters = string.ascii_lowercase
+        result_str = ''.join(random.choice(letters) for i in range(length))
+
+        return result_str
+
+    def getStatus(self, team_id):
         return {
             'status'                  : self.status,
             'question_counter'        : self.question_counter,
@@ -35,8 +57,35 @@ class Game:
             'reading_timer'           : self.reading_timer,
             'thinking_duration'       : self.thinking_duration,
             'thinking_timer'          : self.thinking_timer,
-            'waiting_timer'           : self.waiting_timer
+            'waiting_timer'           : self.waiting_timer,
+            'team_history'            : self.history[team_id]
         }
+
+    def revise(self,teamId, questionId, check):
+        self.history[teamId]['record'][qiestionId] = check
+
+    def updateRankings(self):
+        questionset_structure = [{'q1'},{'q2a','q2b'},{'q3'},{'q4'},{'q5'}]
+        tmp_rankings = {}
+        for teamId in self.history:
+            tmp_rankings[teamId] = [-1 for i in range(len(self.questions_list))]
+        for qno, qstruct in enumerate(questionset_structure):
+            for teamId in self.rankings:
+                teamAnsweredCorrectly = True
+                teamHasntAnsweredYet  = False
+                for questionid in qstruct:
+                    if(self.rankings[teamId]['record'][questionId] == -1):
+                        teamHasntAnsweredYet = True
+                    if(self.rankings[teamId]['record'][questionId] == 0):
+                        teamAnsweredCorrectly = False
+                if(teamHasntAnsweredYet):
+                    rankings[teamId][qno] = -1
+                else:
+                    rankings[teamId][qno] = int(teamAnsweredCorrectly)
+
+
+    def getRankings(self):
+        return self.rankings
 
     def start(self):
         self.status            = 'ongoing'
@@ -50,10 +99,16 @@ class Game:
         self.thinking_timer    = 0
         self.waiting_timer     = 0
 
-        x = threading.Thread(target=self.loop, args=(this))
-        x.start()
-    
-    def verify(self,submission)
+        self.game_thread = threading.Thread(target=self.loop, args=(self))
+        self.game_thread.start()
+
+    def verify(self,submission):
+        teamId     = submission['teamId']
+        questionId = submission['questionId']
+        check      = submission['check']
+        self.history[teamId]['record'][questionId] = check
+
+
     def getNextUnverified(self):
         return self.verification_queue[-1]
 
@@ -104,10 +159,31 @@ class Game:
                 else:
                     self.nextQuestion()
 
-        elif(self.status == 'finished'):
-            print("GAME IS FINISHED")
-            return
+            elif(self.status == 'finished'):
+                print("GAME IS FINISHED")
+                return
 
             print(self.getStatus())
             time.sleep(1)
+
+    def registerTeam(self,team_name):
+        for team_id, team_object in self.history.items():
+            if(team_object['team_name'] == team_name):
+                return {'status' :-1,'message': 'team with this name already exists'}
+
+        team_id = self.get_random_string(5)
+        while(team_id in self.history):
+            team_id = get_random_string(5)
+
+        self.history[team_id] = {
+            'team_id'   : team_id,
+            'team_name' : team_name,
+            'record'    : [0 for q in self.questions_list],
+            'answers'   : {},
+            'score'     : 0
+        }
+
+        self.rankings[team_id] = [0 for q in self.questions_list]
+
+        return {'success':1, 'team_id':team_id}
 
